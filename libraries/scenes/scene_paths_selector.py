ec6d72e0403cb3d559c13ec647ae6069df0eb8b1
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-10-18 13:07:15 +0200

    Create scene_paths_selector.py

---
+++ libraries/scenes/scene_paths_selector.py
@@ -0,0 +1,9 @@
+from libraries.scenes.abstract_scene import AbstractScene
+class ScenePathsSelector(AbstractScene):
+
+    def __init__(self) -> None:
+        pass
+
+    def setup(self) -> None:
+    def destroy(self) -> None:
+        pass
