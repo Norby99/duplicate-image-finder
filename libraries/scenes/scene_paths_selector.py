95cbe8be6139b8065d4b491f6b7bfdc81808b99e
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-10-18 13:30:07 +0200

    added binding

--- libraries/scenes/scene_paths_selector.py
+++ libraries/scenes/scene_paths_selector.py
@@ -1,10 +1,13 @@
 from libraries.scenes.abstract_scene import AbstractScene
 import tkinter as tk
-import tkinter.filedialog
+from tkinter import filedialog
 
 class ScenePathsSelector(AbstractScene):
 
     __window: tk.Tk = None
+
+    __image_folder: str = ""
+    __destination_path: str = ""
 
     def __init__(self) -> None:
         pass
@@ -17,10 +20,17 @@ class ScenePathsSelector(AbstractScene):
         pass
 
     def create_path_widgets(self) -> None:
-        self.btns_path = []
-        for i in ["Source", "Destination"]:
-            self.btns_path.append(tk.Button(self.__window, text=i, command=lambda i=i: self.btns_path_click(tkinter.filedialog.askdirectory())))
-            self.btns_path[-1].pack(pady=5)
+        lbl_image_path = tk.Label(self.__window, text=self.__image_folder)
+        btn_image_path = tk.Button(self.__window, text="Image folder", command=lambda: self.bind_button_to_label(filedialog.askdirectory(), self.__image_folder, lbl_image_path))
 
-            self.label_path = tk.Label(self.__window, text="", font=("Helvetica", 12), background="white")
-            self.label_path.pack(pady=5)
+        lbl_dest_path = tk.Label(self.__window, text=self.__destination_path)
+        btn_dest_path = tk.Button(self.__window, text="Destination folder", command=lambda: self.bind_button_to_label(filedialog.askdirectory(), self.__destination_path, lbl_dest_path))
+
+        btn_image_path.pack(pady=5)
+        lbl_image_path.pack(pady=5)
+        btn_dest_path.pack(pady=5)
+        lbl_dest_path.pack(pady=5)
+        
+    def bind_button_to_label(self, path, attribute, lable) -> None:
+        attribute = path
+        lable.configure(text=attribute)
