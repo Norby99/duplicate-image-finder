7f2de055f5c9d02b66bcbdb72c4a07fdb448993c
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-10-20 14:06:00 +0200

    moved controller

---
+++ libraries/controller/main_controller.py
@@ -0,0 +1,25 @@
+from libraries.view.GUI import GUI
+from libraries.utils.iterator import Iterator
+from libraries.view.scenes.scene_paths_selector import ScenePathsSelector
+import multiprocessing
+
+class MainController:
+
+    __app: GUI = None
+    __application_point: Iterator = Iterator(["file_chooser", "mode_chooser"])
+
+    def __init__(self) -> None:
+        core_count = multiprocessing.cpu_count()
+        self.__app = GUI(set_max_threads=core_count)
+
+        next(self.__application_point)
+        self.update()
+
+        while self.__app.running:
+            self.__app.update()
+
+        self.__app.window.destroy()
+
+    def update(self):
+        if self.__application_point.current == "file_chooser":     # choosing path
+            self.__app.set_scene(ScenePathsSelector())
--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-from libraries.view.GUI import GUI
-from libraries.utils.iterator import Iterator
-from libraries.view.scenes.scene_paths_selector import ScenePathsSelector
-import multiprocessing
+from libraries.controller.main_controller import MainController
 
-class Launcher:
-
-    __app: GUI = None
-    __application_point: Iterator = Iterator(["file_chooser", "mode_chooser"])
-
-    def __init__(self) -> None:
-        core_count = multiprocessing.cpu_count()
-        self.__app = GUI(set_max_threads=core_count)
-
-        next(self.__application_point)
-        self.update()
-
-        while self.__app.running:
-            self.__app.update()
-
-        self.__app.window.destroy()
-
-    def update(self):
-        if self.__application_point.current == "file_chooser":     # choosing path
-            self.__app.set_scene(ScenePathsSelector())
-
-if __name__ == "__main__":
-    launcher = Launcher()
+launcher = MainController()
