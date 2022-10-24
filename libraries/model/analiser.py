381bb40827f65a90369d71361dc2e68bca69246b
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-10-24 16:12:09 +0200

    removed destination path

--- libraries/model/analiser.py
+++ libraries/model/analiser.py
@@ -3,11 +3,12 @@ from libraries.model.abstract_model import AbstractModel
 class Analiser(AbstractModel):
 
     __image_folder: str = ""
-    __destination_path: str = ""
+
+    def __init__(self, image_folder) -> None:
+        self.__image_folder = image_folder
 
     def set_variables(self, image_folder, destination_path) -> None:
-        self.__image_folder = image_folder
-        self.__destination_path = destination_path
+        pass
 
     def analise(self) -> None:
         pass
