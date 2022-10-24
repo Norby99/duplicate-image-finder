2c4fb646fc684999b1942b321b0c4067589fd56e
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-10-24 16:53:22 +0200

    fixed core_count passing

--- libraries/controller/analising_controller.py
+++ libraries/controller/analising_controller.py
@@ -12,16 +12,17 @@ class AnalisingController(AbstractController):
     __model: Analiser = None
     __image_folder: str = ""
 
-    def __init__(self, image_folder) -> None:
+    def __init__(self, max_threads, image_folder) -> None:
         self.__image_folder = image_folder
 
         self.__scene = SceneAnaliser()
-        self.__model = Analiser(self.__image_folder)
+        self.__model = Analiser(max_threads, self.__image_folder)
 
         print(self.__image_folder)
 
         self.__scene.set_text("Loading images...")
         self.__model.analise()
+        self.__scene.set_text("Done")
 
     def get_model(self) -> AbstractModel:
         return self.__model
