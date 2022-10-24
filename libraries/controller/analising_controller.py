2919a93048a0ede9c74a4669bcbc602ad0b108f8
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-10-24 15:50:03 +0200

    first working setup

--- libraries/controller/analising_controller.py
+++ libraries/controller/analising_controller.py
@@ -1,5 +1,22 @@
 from libraries.controller.abstract_controller import AbstractController
 
+from libraries.view.scenes.abstract_scene import AbstractScene
+from libraries.view.scenes.scene_analiser import SceneAnaliser
+
+from libraries.model.abstract_model import AbstractModel
+from libraries.model.analiser import Analiser
+
 class AnalisingController(AbstractController):
 
-    pass
+    __scene: SceneAnaliser = None
+    __model: Analiser = None
+
+    def __init__(self) -> None:
+        self.__scene = SceneAnaliser()
+        self.__model = Analiser()
+
+    def get_model(self) -> AbstractModel:
+        return self.__model
+
+    def get_scene(self) -> AbstractScene:
+        return self.__scene
