cf7240643407c921b40cf3e58052f1ec46992f5b
Norbert Gabos <dont.vip@gmail.com>
2023-05-26 18:12:40 +0200

    added path and memory optimization
    now image is loaded only on the method call

--- libraries/utils/image_data.py
+++ libraries/utils/image_data.py
@@ -1,20 +1,25 @@
 import tkinter as tk
+from PIL import Image
 
 class ImageData:
      
-    __image: tk.PhotoImage = None
+    __image_path: tk.PhotoImage = None
     __image_name: str = None
     __image_size: str = None
     __image_dimensions: tuple = None
 
-    def __init__(self, image: tk.PhotoImage, image_name: str, image_size: str, image_dimensions: tuple) -> None:
-        self.__image = image
+    def __init__(self, image_path: str, image_name: str, image_size: str, image_dimensions: tuple) -> None:
+        self.__image_path = image_path
         self.__image_name = image_name
         self.__image_size = image_size
         self.__image_dimensions = image_dimensions
     
     def get_image(self) -> tk.PhotoImage:
-        return self.__image
+        # the image is not loaded into memory until this method is called
+        return Image.open(self.__image_path)
+    
+    def get_image_path(self) -> str:
+        return self.__image_path
 
     def get_image_name(self) -> str:
         return self.__image_name
