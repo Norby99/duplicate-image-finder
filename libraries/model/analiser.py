3f934048c8748e4be22d494f632b997449143f12
Norbert Gabos <dont.vip@gmail.com>
2023-05-26 19:01:08 +0200

    converted image analize to ImageData

--- libraries/model/analiser.py
+++ libraries/model/analiser.py
@@ -1,11 +1,12 @@
 from libraries.model.abstract_model import AbstractModel
+from libraries.utils.image_data import ImageData
 
 import imagehash
 from PIL import Image
-
+from collections import defaultdict
 from threading import Thread
 
-from os import listdir, path
+from os import listdir, path, stat
 import warnings
 
 class Analiser(AbstractModel):
@@ -14,7 +15,7 @@ class Analiser(AbstractModel):
     __image_extension: tuple = ('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')
     __ready: bool = False
     __max_threads: int = 1
-    __images: list = []
+    __images: list[ImageData] = []
 
     def __init__(self, core_count, image_folder) -> None:
         self.__image_folder = image_folder
@@ -23,23 +24,33 @@ class Analiser(AbstractModel):
     def set_variables(self, image_folder, destination_path) -> None:
         pass
 
-    def analise(self) -> None:
+    def load_images(self) -> None:
         images_path = self.__get_images_path()
         thread_list = []
         chunk_size = int(len(images_path) / self.__max_threads)
 
         for i in range(0, len(images_path), chunk_size):
-            thread_list.append(Thread(target=self.__load_images, args=(images_path[i:i + chunk_size],)))
+            thread_list.append(Thread(target=self.__threadable_load_images, args=(images_path[i:i + chunk_size],)))
             thread_list[-1].start()
         
         for thread in thread_list:
             thread.join()
-        print("Done")
 
-    def __load_images(self, images_path: list) -> None:
+    def compare_images(self) -> None:
+        #old_var = [images_name[i] for i, x in enumerate(images_hash) if images_hash.count(x) > 1] # O(n^2)
+        #TODO: investigate for better performance
+        old_var = defaultdict(list) # O(n)
+        for i in self.__images:
+            old_var[i.get_hash()].append(i)
+        old_var = {k:v for k,v in old_var.items() if len(v)>1}
+
+        return [[i for i in x] for x in old_var.values()]   # changing the index to the name outside of the loop above is a bit faster
+
+    def __threadable_load_images(self, images_path: list) -> None:
         for img in images_path:
             try:
-                self.__images.append([imagehash.average_hash(Image.open(img[0])), img[1]])
+                image = Image.open(img[0])
+                self.__images.append(ImageData(img[0], img[1], stat(img[0]).st_size, image.size, imagehash.average_hash(image)))
             except Exception as e:
                 print(e.args)
                 warnings.warn(("Probably the file is damaged: ", img[1]))
