2aff9a40b5584c3615ee81e3f1252dd9f4c31a1b
Norbert Gabos <dont.vip@gmail.com>
2023-07-02 22:01:28 +0200

    added skip button

--- libraries/view/scenes/scene_result.py
+++ libraries/view/scenes/scene_result.py
@@ -4,13 +4,17 @@ from libraries.utils.image_container_group import ImageContainerGroup
 import tkinter as tk
 
 from libraries.controller.observable import Observable
+
+class SceneResult(AbstractScene, Observable):
     
         __window: tk.Tk
 
         __image_remover: Observable
+
         __img1: ImageContainerGroup
         __img2: ImageContainerGroup
     
+        __btn_skip: tk.Button
 
         __extend_window_size: list[int] = [1200, 1000]
         __last_size: list[int] = [0, 0]
@@ -20,24 +24,26 @@ from libraries.controller.observable import Observable
 
         def setup(self, window: tk.Tk) -> None:
             self.__window = window
+            self.__create_widgets()
             self.__window.geometry(str(self.__extend_window_size[0]) + "x" + str(self.__extend_window_size[1]))
-            #self.__packWidgets()
 
         def set_image_remover(self, image_remover: Observable) -> None:
             self.__image_remover = image_remover
 
         def set_images(self, image1: ImageData, image2: ImageData) -> None:
+            self.__img1 = ImageContainerGroup(image1, self.__window)
             self.__img1.setup_widgets([0, 0], "w")
 
+            self.__img2 = ImageContainerGroup(image2, self.__window)
             self.__img2.setup_widgets([0, 1], "e")
 
-            self.__set_image2(images[1])
             self.resize_window_elements()
 
         def resize_window_elements(self) -> None:
             self.__img1.resize()
             self.__img2.resize()
 
+            self.__btn_skip.place(x=self.__window.winfo_width()/2, y=self.__window.winfo_height() - 20, anchor='center')
     
         def __create_widgets(self) -> None:
             self.__btn_skip = tk.Button(self.__window, text="Skip", command=lambda: self.__send_form())
@@ -53,3 +59,5 @@ from libraries.controller.observable import Observable
 
         def destroy(self) -> None:
             self.__btn_skip.destroy()
+            self.__img1.destroy()
+            self.__img2.destroy()
